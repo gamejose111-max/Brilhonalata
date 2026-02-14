1790455b3ac68f83df4cd94a0fd3a1ff273cf877
José Games <gamejose111@gmail.com>
2026-02-14 01:59:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6025642929-d3155",
+  "appId": "1:884842461006:web:7d197473c8a0442973d01f",
+  "apiKey": "AIzaSyAYsd10HFITcgBOHkYzJyyhFc6XLlv4UTA",
+  "authDomain": "studio-6025642929-d3155.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "884842461006"
+};
